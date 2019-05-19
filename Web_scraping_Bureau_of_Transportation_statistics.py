9992539e2aaee6ba3bf406ae9ad383a0225a96a0
Gaurav Rai <gauravrai@tamu.edu>
2019-05-18 21:13:57 -0500

    Update Web_scraping_Bureau_of_Transportation_statistics.py

--- Web_scraping_Bureau_of_Transportation_statistics.py
+++ Web_scraping_Bureau_of_Transportation_statistics.py
@@ -5,3 +5,9 @@ Created on Sat May 18 21:10:40 2019
 @author: gaura
 """
 
+from bs4 import BeautifulSoup
+soup= BeautifulSoup(open("virgin_and_logan_airport.html"))
+option_values=[]
+carrier_list= soup.find(id="CarrierList")
+for option in carrier_list.find_all('option'):
+    option_values.append(option['value'])
