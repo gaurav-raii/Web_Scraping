f82cde5013712a8aa5075bf2133f3d5458cb1502
Gaurav Rai <gauravrai@tamu.edu>
2019-05-21 12:23:12 -0500

    Update Web_scraping_Bureau_of_Transportation_statistics.py

--- Web_scraping_Bureau_of_Transportation_statistics.py
+++ Web_scraping_Bureau_of_Transportation_statistics.py
@@ -35,3 +35,4 @@ eventvalidation= ev["value"]
 r=s.post("https://www.transtats.bts.gov/Data_Elements.aspx?Data=2")
 data={'AirportList': "BOS",
       'CarrierList': "VX",
+      'Submit': 'Submit',
